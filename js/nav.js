27da21a5519508ef5d691d3abd8bbd864821a2ce
GarfieldMSB <matias.sepulveda.bustamante@gmail.com>
2023-03-06 01:49:31 -0300

    Navbar

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,20 @@
 const nav = document.querySelector('.nav-menu');
+const navContainer = document.querySelector('.navbar-container');
 
 const Nav = () => {
 
     nav.addEventListener('click', changeIcon);
+    navContainer.addEventListener('click', closeNavbar)
 
     function changeIcon() {
         nav.classList.toggle('active');
+    }
+
+    function closeNavbar(e) {
+        if (e.target !== this)
+        return; 
+        nav.classList.remove('active');
+        console.log('Click');
     }
 };
 
