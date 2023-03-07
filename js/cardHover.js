e41cdf03b3efbd1db918cf7f6d00c90207221b56
GarfieldMSB <matias.sepulveda.bustamante@gmail.com>
2023-03-07 20:03:57 -0300

    Close function navbar-responsive

--- js/cardHover.js
+++ js/cardHover.js
@@ -5,16 +5,25 @@ const cardDetails = document.querySelector('.card-details');
 export const CardHover = () => {
 
     //Event Listeners
+
     cards.forEach(card => {
-        card.addEventListener('mouseover', (e) => {
+        card.addEventListener('mouseover', () => {
             card.childNodes[3].classList.add('active');
         })
-    })
-    cards.forEach(card => {
-        card.addEventListener('mouseout', (e) => {
+
+        card.addEventListener('mouseout', () => {
+            card.childNodes[3].classList.remove('active');
+        })
+
+        card.addEventListener('touchstart', () => {
+            card.childNodes[3].classList.add('active');
+        })
+
+        card.addEventListener('touchend', () => {
             card.childNodes[3].classList.remove('active');
         })
     })
+    
 
     //Functions 
     function showDetails(e) {
