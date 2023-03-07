d061e9200d72c75fe2b3ef46b48f71b09b23e57c
GarfieldMSB <matias.sepulveda.bustamante@gmail.com>
2023-03-07 18:31:31 -0300

    Close function navbar-responsive

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,29 @@
 const nav = document.querySelector('.nav-menu');
-const navContainer = document.querySelector('.navbar-container');
+const navCheck = document.querySelector('#nav-check');
 
 const Nav = () => {
 
+    //Eventlistener
+    // Close navbar when click event is out of nav element
+    window.addEventListener('click', closeNavbar);
+
+    // Change menu icon
     nav.addEventListener('click', changeIcon);
 
+
+    // Functions
     function changeIcon() {
         nav.classList.toggle('active');
+    }
+
+    function closeNavbar(e) {
+        if (navCheck.checked) {
+            if (e.target.nodeName === 'LI' || e.target.nodeName === 'NAV' || e.target.nodeName === 'A' || e.target.id === 'nav-check') {
+                return;
+            } else {
+                navCheck.checked = false;
+            }
+        }
     }
 
 };
