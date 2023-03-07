114add19809caaadd0068b3bea11cb25b9fd7ff9
GarfieldMSB <matias.sepulveda.bustamante@gmail.com>
2023-03-07 20:23:19 -0300

    Close function navbar-responsive

--- js/cardHover.js
+++ js/cardHover.js
@@ -8,18 +8,23 @@ export const CardHover = () => {
     //Event Listeners
 
     cards.forEach(card => {
+
+        // When the mouse is on the card, the card details are displayed
         card.addEventListener('mouseover', () => {
             card.childNodes[3].classList.add('active');
         })
 
+        // When the mouse is out of the card, the card details are disable
         card.addEventListener('mouseout', () => {
             card.childNodes[3].classList.remove('active');
         })
 
+        // When the mobile mouse is on the card, the card details are displayed
         card.addEventListener('touchstart', () => {
             card.childNodes[3].classList.add('active');
         })
 
+        // When the mobile mouse touch out of the card, the card details are disable
         window.addEventListener('touchstart', (e) => {
             if (card.contains(e.target)) {
                 return;
@@ -28,13 +33,4 @@ export const CardHover = () => {
             }
         });
     })
-    
-
-    //Functions
-    function showDetails(e) {
-        cardDetails.classList.add('active')
-    }
-    function closeDetails(e) {
-        cardDetails.classList.remove('active')
-    }
 }
--- js/upBtn.js
+++ js/upBtn.js
@@ -3,18 +3,20 @@ const up = document.querySelector('.up');
 
 export const Up = () => {
     
+    // When windows scroll in cross axis is > 200 up button is displayed
     window.onscroll = function() {
         let y = window.scrollY;
         if (y > 200) {
             up.setAttribute('style', 'display: flex');
-            up.setAttribute('data-aos', 'fade-left');
         } else {
             up.setAttribute('style', 'display: none')
         }
     }
 
+    // Event listeners
     up.addEventListener('click', upWeb);
 
+    // Functions
     function upWeb() {
         window.scroll(0, 0);
     }
